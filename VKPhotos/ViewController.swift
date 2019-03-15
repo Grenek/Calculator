d979d6380151421258c7e939d600fde6e75a1fa2
Grenek <grenek075@gmail.com>
2019-03-15 16:26:59 +0500

    Initial Commit

---
+++ VKPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKPhotos
+//
+//  Created by Вадим on 15.03.2019.
+//  Copyright © 2019 Shamratov Vadim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
