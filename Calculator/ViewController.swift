793c6fba58fe535b19bf9c830da341533a9c73fe
Grenek <grenek075@gmail.com>
2019-04-23 19:27:56 +0500

    comments

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UITextField!
     
     @IBAction func numbers(_ sender: UIButton) {
+        
         // Проверка что выводить в label.text при различных условиях
         if pointPressed == true {
             label.text = label.text! + String(sender.tag-1)
@@ -44,6 +45,7 @@ class ViewController: UIViewController {
     @IBAction func buttons(_ sender: UIButton) {
         
         switch sender.tag {
+            
         case 11: // Del button
             label.text = ""
             mathInProgress = false
@@ -56,6 +58,7 @@ class ViewController: UIViewController {
             previousNumber = 0
             currentNumber = 0
             calculatedNumber = 0
+            
         case 12: // Divide button
             guard label.text != "" && label.text != "." else {break}
             dividePressed = true
@@ -65,12 +68,13 @@ class ViewController: UIViewController {
             mathInProgress = true
             pointPressed = false
             
-            // Проверка если число на экране это наш округленный calculatedNumber
+            // Проверка что число на экране == округленный calculatedNumber для точности расчетов
             if label.text == String(round(1000000*calculatedNumber)/1000000) {
                 previousNumber = calculatedNumber
             } else {
                 previousNumber = Double(self.label.text!)!
             }
+            
         case 13: // Multiply button
             guard label.text != "" && label.text != "." else {break}
             dividePressed = false
@@ -80,12 +84,13 @@ class ViewController: UIViewController {
             mathInProgress = true
             pointPressed = false
             
-            // Проверка если число на экране это наш округленный calculatedNumber
+            // Проверка что число на экране == округленный calculatedNumber для точности расчетов
             if label.text == String(round(1000000*calculatedNumber)/1000000) {
                 previousNumber = calculatedNumber
             } else {
                 previousNumber = Double(self.label.text!)!
             }
+            
         case 14: // Substract button
             guard label.text != "" && label.text != "." else {break}
             dividePressed = false
@@ -95,6 +100,7 @@ class ViewController: UIViewController {
             mathInProgress = true
             pointPressed = false
             previousNumber = Double(self.label.text!)!
+            
         case 15: // Add button
             guard label.text != "" && label.text != "." else {break}
             dividePressed = false
@@ -104,6 +110,7 @@ class ViewController: UIViewController {
             mathInProgress = true
             pointPressed = false
             previousNumber = Double(self.label.text!)!
+            
         case 16: // Equals button
             guard label.text != "" && label.text != "." else {break}
             currentNumber = Double(self.label.text!)!
@@ -127,12 +134,15 @@ class ViewController: UIViewController {
                 plusPressed = false
             }
             let isInteger = floor(calculatedNumber) == calculatedNumber
+            
+            // Выводим значение без .0 если число целое
             if isInteger == true {
                 label.text = String(format: "%.0f", calculatedNumber)
             } else {
                 label.text = String(round(1000000*calculatedNumber)/1000000)
             }
-        case 17:
+            
+        case 17: // логика нажатия на ","
             pointPressed = true
             if label.text?.contains(".") == false && mathInProgress == false {
                 label.text = label.text! + "."
@@ -140,15 +150,14 @@ class ViewController: UIViewController {
                 label.text = "0."
                 mathInProgress = false
             }
-            
         default: break
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         label.isUserInteractionEnabled = false
     }
-    
     
 }
 
